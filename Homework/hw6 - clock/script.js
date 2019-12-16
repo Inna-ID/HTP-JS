e01417cbabca0f45240e7fe2b6d26121aee89c1a
Inna-ID <gmccsilv@gmail.com>
2019-12-16 23:57:54 +0300

    done hw 6 - clock

--- Homework/hw6 - clock/script.js
+++ Homework/hw6 - clock/script.js
@@ -1,3 +1,9 @@
+var arrows = {
+	second: document.getElementsByClassName('sec-arrow')[0],
+	minute: document.getElementsByClassName('min-arrow')[0],
+	hour: document.getElementsByClassName('hour-arrow')[0]
+}
+
 function getTime() {
 	var date = new Date(),
 		hour = date.getHours(),
@@ -5,7 +11,17 @@ function getTime() {
 		second = date.getSeconds();
 
 	writeTime(hour, minute, second);
+
+	//for smooth movement of arrows
+	//current minute or second / 60 * the angle of arrow rotation
+	var hourAdditionalDegree = (minute / 60) * 30;
+	var minuteAdditionalDegree = (second / 60) * 6;
+
+	rotateArrow(second, arrows.second, 6, 0);
+	rotateArrow(minute, arrows.minute, 6, minuteAdditionalDegree);
+	rotateArrow(hour, arrows.hour, 30, hourAdditionalDegree);
 }
+
 
 function writeTime(h, m, s) {
 	var container = document.getElementById('clock');
@@ -16,8 +32,10 @@ function writeTime(h, m, s) {
 	container.innerHTML = `${h}:${m}:${s}`;
 }
 
-setInterval(getTime, 1000);
-//// find time end
+
+function rotateArrow(t, arrow, multiplicity, additionalDegree) {
+	arrow.style.transform = `rotate(${t * multiplicity + additionalDegree}deg)`;
+}
 
 
 function drawNums() {
@@ -41,4 +59,7 @@ function drawNums() {
 		container.appendChild(wrap);
 	}
 }
+
 drawNums();
+getTime();
+setInterval(getTime, 1000);
