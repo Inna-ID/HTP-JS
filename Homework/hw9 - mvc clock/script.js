7fb6abe136f20d84ad80050040c436a742f7ff85
O-DANILEVICH\Inna <gmccsilv@gmail.com>
2020-01-21 17:39:35 +0300

    hw MVC some changes

--- Homework/hw9 - mvc clock/script.js
+++ Homework/hw9 - mvc clock/script.js
@@ -1,24 +1,37 @@
+"use strict";
+
 // model
 let time = {
-	date: new Date(),
+	ms: new Date().getTime(),
+	msPerSecond: 1000,
+	msPerMinute: 6000,
+	msPerHour: 3600000,
 	// gtm: 3,
 	// start: false,
 	// stop: true,
-
-	updateView() {
-		pageView.update()
+	convertMS: function() {
+		return {
+			hour: Math.floor(this.ms / this.msPerHour),
+			minute: Math.floor(this.ms / this.msPerMinute),
+			second: Math.floor(this.ms / this.msPerHour)
+		}
 	},
-	updateTime() {
-		this.date = new Date();
-		console.log(this.date)
+	updateView: function() {
+		let date = this.convertMS();
+		pageView.update(date);
+	},
+	updateTime: function() {
+		this.ms = new Date().getTime();
 		this.updateView();
 	}
 }
+setInterval(time.updateTime, 1000);
+
 
 // view
 let pageView = {
-	update() {
-		document.getElementsByClassName('time')[0].innerHTML = time.date;
+	update: function(curTime) {
+		document.getElementsByClassName('time')[0].innerHTML = `${curTime.hour} : ${curTime.minute} : ${curTime.second}`;
 	}
 }
 
